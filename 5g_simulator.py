e548a81c42287ab25139ca0f25f09eb6ae8b8613
lamotnat <nathanl4school@gmail.com>
2023-10-28 16:58:23 -0400

    5g wrapper

---
+++ 5g_simulator.py
@@ -0,0 +1,15 @@
+class fiveG_Network:
+    '''Simulates a 5g network for the cars and MEC to be placed into.'''
+
+    def __init__(self, mec_obj):
+        self.mec = mec_obj
+        self.obu_list = set()
+
+    def brodcast(self, message):
+        self.mec.input_message(message)
+        #TODO make mec input
+
+    def publish_event(self, update):
+        for car in self.obu_list:
+            car.input_update(update)
+            #TODO make car input update
